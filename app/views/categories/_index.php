41589f66d518933dcdbb2abcf12036c05caecfc3
MN <bmdaetl@gmail.com>
2025-05-22 12:49:52 +0200

    Search form et categories/_index dans le _aside

---
+++ app/views/categories/_index.php
@@ -0,0 +1,15 @@
+<?php
+    include_once '../app/models/categoriesModel.php';
+    $categories = \App\Models\categoriesModel\findAll($connexion);
+?>
+<h2 class="text-lg font-bold mb-2">Categories</h2>
+<ul class="list-disc pl-5 space-y-1">
+    <?php foreach($categories as $category): ?>
+        <li>
+            <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                <?php echo $category['name']; ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+    <!-- Répétez pour d'autres catégories -->
+</ul>
